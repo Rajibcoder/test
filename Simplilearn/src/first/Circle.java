6eba16b5909e9b368ee934818840470c7ab21e8a
Rajibcoder <rajibkumarmaity1110@gmail.com>
2023-07-06 13:56:52 +0530

    commit cicle

---
+++ Simplilearn/src/first/Circle.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Circle extends Shape{
+	
+private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    public void calculateArea() {
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle: " + area);
+    }
+
+}
